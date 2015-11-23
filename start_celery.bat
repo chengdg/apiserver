4269efc57a4bfcd4b19e5c9bd72ff3e1df0f7fdd
郭玉成 <1597594211@qq.com>
2015-11-23 14:31:24 +0800

    修改windows下celery启动脚本

--- start_celery.bat
+++ start_celery.bat
@@ -1,2 +1,2 @@
-celery -A apiserver worker -l info --pidfile="celery.pid"
-REM celery worker -A apiserver --pidfile="celery-%N-%i.pid" --logfile="celery-%N-%i.log" -l info
+celery -A core.celery worker -l info --pidfile="celery.pid"
+REM celery core.celery -A apiserver --pidfile="celery-%N-%i.pid" --logfile="celery-%N-%i.log" -l info

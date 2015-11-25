0a0b1f7fa192a5afdf1ba91d8cfb51e4f91abafb
江哲 <jiangzhe@weizoom.com>
2015-11-25 15:21:09 +0800

    统一商城wapi结构

--- wapi/mall/__init__.py
+++ wapi/mall/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import product
+import a_product_detail
+import a_products
+import a_product_stocks
+import a_product
+import a_product_categories
 import a_shopping_cart_item
 import a_shopping_cart
 import a_purchasing

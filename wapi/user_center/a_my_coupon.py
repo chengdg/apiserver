9288ece0624749784fb4041b818580c4cd76e708
朱天琦 <zhutianqi@weizoom.com>
2015-12-18 16:12:19 +0800

    [master][朱天琦]修复个人中心-我的优惠券wapi接口

--- wapi/user_center/a_my_coupon.py
+++ wapi/user_center/a_my_coupon.py
@@ -19,21 +19,20 @@ class AMyCoupon(api_resource.ApiResource):
 		获取个人中心
 		"""
 		webapp_user = args['webapp_user']
-		coupons = Coupon.get_coupons_by_webapp_user({'webapp_user': webapp_user})
+		coupons = webapp_user.coupons
 		used_coupons = []
 		unused_coupons = []
 		expired_coupons = []
 
 		for c in coupons:
-			c.consumer_name = ''
 			if c.status == promotion_models.COUPON_STATUS_USED:
-				used_coupons.append(c)
+				used_coupons.append(c.to_dict())
 
 			if c.status == promotion_models.COUPON_STATUS_UNUSED:
-				unused_coupons.append(c)
+				unused_coupons.append(c.to_dict())
 
 			if c.status == promotion_models.COUPON_STATUS_EXPIRED:
-				expired_coupons.append(c)
+				expired_coupons.append(c.to_dict())
 
 		return {
 			'used_coupons': used_coupons,

ecc2635f1fb835825b27fb19e466233d11d54c14
duhao <duhao@weizoom.com>
2015-12-23 10:51:26 +0800

    update

--- business/wzcard/wzcard_resource_allocator.py
+++ business/wzcard/wzcard_resource_allocator.py
@@ -9,7 +9,7 @@ from business.wzcard.wzcard import WZCard
 from business.wzcard.wzcard_resource import WZCardResource
 from business.wzcard.wzcard_checker import WZCardChecker
 from decimal import Decimal
-from business.mall import log_operator
+from business.mall.log_operator import LogOperator
 
 
 # 每单用微众卡数量
@@ -116,7 +116,7 @@ class WZCardResourceAllocator(business_model.Service):
 			#记录微众卡消费日志 duhao
 			#TODO 这种方式太low了。。。
 			for item in used_wzcards:
-				log_operator.record_wzcard_log(self.__webapp_owner.id, order.order_id, item[0].id, used_amount)
+				LogOperator.record_wzcard_log(self.__webapp_owner.id, order.order_id, item[0].id, used_amount)
 		else:
 			# 退还微众卡
 			for item in used_wzcards:

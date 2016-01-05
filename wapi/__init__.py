f62d71a41171c7484e2c09b977a61537563be60e
victor <gaoliqi@weizoom.com>
2016-01-05 08:58:28 +0000

    开启WAPI logger。记录到MongoDB。

--- wapi/__init__.py
+++ wapi/__init__.py
@@ -8,6 +8,9 @@ from wapi_utils import wapi_log
 import datetime as dt
 import time
 import logging
+from wapi import wapi_utils
+from core import api_resource
+import json
 
 """
 wapi_path = os.path.join(settings.PROJECT_HOME, '..', 'wapi')
@@ -22,55 +25,30 @@ for f in os.listdir(wapi_path):
 		module = __import__(module_name, {}, {}, ['*',])
 """
 
-from core import api_resource
-import json
-#import resources
-
 class ApiNotExistError(Exception):
 	pass
-
-def _param_to_text(param):
-	"""
-	将param转成JSON text，用于调试
-
-	@todo to be optimized
-	"""
-	new_param = {}
-	for key, value in param.items():
-		if key == 'webapp_user':
-			new_param['wid'] = param['webapp_user'].id
-		elif key == 'webapp_owner':
-			new_param['woid'] = param['webapp_owner'].id
-		else:
-			new_param[key] = value
-	return json.dumps(new_param)
 
 
 def wapi_call(method, app, resource, data, req=None):
 	resource_name = resource
 	key = '%s-%s' % (app, resource)
 	#if settings.WAPI_LOGGER_ENABLED:
-	logging.info("called WAPI: {} {}/{}, param: {}".format(method, app, resource, _param_to_text(data)))
+	if settings.MODE == 'develop':
+		logging.info("called WAPI: {} {}/{}, param: {}".format(method, app, resource, wapi_utils.param_to_text(data)))
 
 	#start_at = dt.datetime.now()
 	start_at = time.clock()
 	resource = api_resource.APPRESOURCE2CLASS.get(key, None)
 	if not resource:
-		#timediff = dt.datetime.now() - start_at
-		#wapi_log(app, resource_name, method, data, timediff.total_seconds(), -1)
 		wapi_log(app, resource_name, method, data, (time.clock()-start_at), -1)
 		raise ApiNotExistError('%s:%s' % (key, method))
 
 	func = getattr(resource['cls'], method, None)
 	if not func:
-		#timediff = dt.datetime.now() - start_at
-		#wapi_log(app, resource_name, method, data, timediff.total_seconds(), -1)
 		wapi_log(app, resource_name, method, data, (time.clock()-start_at), -1)
 		raise ApiNotExistError('%s:%s' % (key, method))
 
 	response = func(data)
-	#timediff = dt.datetime.now() - start_at
-	#wapi_log(app, resource_name, method, data, timediff.total_seconds(), 0)
 	wapi_log(app, resource_name, method, data, (time.clock()-start_at), 0)
 	return response
 
--- wapi/wapi_utils.py
+++ wapi/wapi_utils.py
@@ -4,9 +4,34 @@ from wapi.logger.mongo_logger import MongoAPILogger
 import wapi
 import settings
 import logging
+import json
 
 
 _wapi_logger = None
+
+def morph_params(param):
+	"""
+	将`webapp_user`和`webapp_owner`变成`wid`和`woid`
+
+	@todo to be optimized
+	"""
+	new_param = {}
+	for key, value in param.items():
+		if key == 'webapp_user':
+			new_param['wid'] = param['webapp_user'].id
+		elif key == 'webapp_owner':
+			new_param['woid'] = param['webapp_owner'].id
+		else:
+			new_param[key] = value
+	return new_param
+
+
+def param_to_text(param):
+	"""
+	将param转成JSON text，用于调试
+	"""
+	return json.dumps(morph_params(param))
+
 
 def wapi_log(app, resource, method, params, time_in_s, status=0):
 	"""
@@ -17,15 +42,8 @@ def wapi_log(app, resource, method, params, time_in_s, status=0):
 		if _wapi_logger is None:
 			_wapi_logger = MongoAPILogger()
 		if settings.MODE == 'develop' or settings.MODE == 'test':
-			logging.info("called WAPI (in {} s): {} {}/{}, param: {}".format(time_in_s, method, app, resource, wapi._param_to_text(params)))
-		return _wapi_logger.log(app, resource, method, params, time_in_s, status)
+			logging.info("called WAPI (in {} s): {} {}/{}, param: {}".format(time_in_s, method, app, resource, param_to_text(params)))
+		return _wapi_logger.log(app, resource, method, morph_params(params), time_in_s, status)
 	else:
-		logging.info("called WAPI (in {} s): {} {}/{}, param: {}".format(time_in_s, method, app, resource, wapi._param_to_text(params)))
+		logging.info("called WAPI (in {} s): {} {}/{}, param: {}".format(time_in_s, method, app, resource, param_to_text(params)))
 	return
-
-
-def get_webapp_id_via_oid(owner_id):
-	"""
-	@TODO to be implemented
-	"""
-	return 0

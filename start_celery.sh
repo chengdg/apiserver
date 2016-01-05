714e3655d2d5f1fc2a7e5bf7d11b3433368c98c3
victor <gaoliqi@weizoom.com>
2016-01-05 18:12:33 +0800

    update

--- start_celery.sh
+++ start_celery.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #celery -A weapp worker -l info
 rm -f celery.pid
-nohup python run_celery.py   &
+#nohup python run_celery.py   &
+celery -A core.service worker -l info --pidfile="celery.pid"

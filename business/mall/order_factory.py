0cfb200aa37fa5ee82b488ee317694636ec759f8
朱天琦 <zhutianqi@weizoom.com>
2016-02-29 11:53:58 +0800

    [master][朱天琦]更正支付方式为货到付款的判断

--- business/mall/order_factory.py
+++ business/mall/order_factory.py
@@ -240,7 +240,8 @@ class OrderFactory(business_model.Model):
 		if order.final_price == 0:
 			# 优惠券或积分金额直接可支付完成，直接调用pay_order，完成支付
 			order.pay(mall_models.PAY_INTERFACE_PREFERENCE)
-		elif order.pay_interface_type == mall_models.PAY_INTERFACE_COD:
+
+		elif int(order.pay_interface_type) == mall_models.PAY_INTERFACE_COD:
 			# 执行货到付款的支付操作
 			order.pay(mall_models.PAY_INTERFACE_COD)
 		return order

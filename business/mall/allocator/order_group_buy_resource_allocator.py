1881a6c52eee772319f1da0455cda75eb4e46df5
朱天琦 <zhutianqi@weizoom.com>
2016-03-11 15:57:57 +0800

    [group][朱天琦]更新下单检测

--- business/mall/allocator/order_group_buy_resource_allocator.py
+++ business/mall/allocator/order_group_buy_resource_allocator.py
@@ -23,70 +23,67 @@ class OrderGroupBuyAllocator(business_model.Service):
 		self.context['webapp_user'] = webapp_user
 
 	def allocate_resource(self, order, purchase_info):
+
+		# 检测是否团购订单
 		if not purchase_info.group_id:
 			return self.__return_empty_resource()
 
 		# 检测purchase_info互斥
+		is_success, reason = self.__check_purchase_info(purchase_info)
+
+		# todo 检测是否重复下单
+
+		pid = order.products[0].id
+
+		if is_success:
+			# 申请资源
+			import requests
+			url = 'http://' + settings.WEAPP_DOMAIN + '/m/apps/group/api/check_group_buy'
+			param_data = {
+				'member_id': self.context['webapp_user'].member.id,
+				'group_id': purchase_info.group_id,
+				'pid': pid,
+				'woid': self.context['webapp_owner'].id
+			}
+			r = requests.get(url=url,params=param_data)
+			print '*******************************************************'
+			print(r.text)
+			print '*******************************************************'
+			group_buy_product_info = json.loads(r.text)['data']
+			is_success = group_buy_product_info['is_success']
+			reason = group_buy_product_info['reason']
 
 
-		group_buy_product_id = order.products[0].id
+		# mock_group_buy_product_info = {
+		# 	'is_success': True,
+		# 	'group_buy_price': 200,
+		# 	'reason': 'asdasdasdasda',
+		# }
 
-		# 检测活动可行性
+		#
+		# group_buy_product_info = mock_group_buy_product_info
 
-		# 申请资源
+		if is_success:
+			group_buy_resource = GroupBuyResource.get({
+				'type': self.resource_type,
+			})
 
-		import requests
-		url = 'http://' + settings.WEAPP_DOMAIN + '/m/apps/group/api/check_group_buy'
-		param_data = {
-			'member_id': self.context['webapp_user'].member.id,
-			'group_id': purchase_info.group_id,
-			'pid': group_buy_product_id,
-			'woid': self.context['webapp_owner'].id
-		}
-		r = requests.get(url=url,params=param_data)
-		print '*******************************************************'
-		print(r.text)
-		print '*******************************************************'
-		group_buy_product_info = json.loads(r.text)['data']
+			group_buy_resource.pid = pid
+			group_buy_resource.group_buy_price = group_buy_product_info['group_buy_price']
 
-
-		mock_group_buy_product_info = {
-			'is_success': True,
-			'group_buy_price': 200,
-			'reason': 'asdasdasdasda',
-		}
-
-
-		group_buy_product_info = mock_group_buy_product_info
-
-
-
-		if not group_buy_product_info['is_success']:
+			return True, '', group_buy_resource
+		else:
 			# 申请资源失败
 			reason_dict = {
 				"is_success": False,
-				"msg": group_buy_product_info['reason'],
+				"msg": reason,
 				"type": "group_buy"
 			}
 			return False, [reason_dict], None
 
-
-
-		group_buy_resource = GroupBuyResource.get({
-			'type': self.resource_type,
-		})
-
-		group_buy_resource.pid = group_buy_product_id
-		group_buy_resource.group_buy_price = group_buy_product_info['group_buy_price']
-
-		return True, '', group_buy_resource
-
-
-
-
 	def release(self, resource):
 		if resource.get_type() == self.resource_type:
-			# 通知团购服务
+			# 无资源需要释放
 			pass
 
 	@property
@@ -94,8 +91,14 @@ class OrderGroupBuyAllocator(business_model.Service):
 		return business_model.RESOURCE_TYPE_GROUP_BUY
 
 	def __return_empty_resource(self):
-		print('-----x')
 		empty_coupon_resource = GroupBuyResource.get({
 			'type': self.resource_type,
 		})
-		return True, '',empty_coupon_resource
+		return True, '', empty_coupon_resource
+
+	def __check_purchase_info(self, purchase_info):
+		# 团购订单不能使用积分、优惠券
+		if purchase_info.order_integral_info or purchase_info.group2integralinfo or purchase_info.coupon_id:
+			return False, u'团购订单不能使用积分、优惠券'
+		else:
+			return True, u''

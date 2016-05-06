3d8c1995f56aae4af1632b951eb6f4030d8b67b4
朱天琦 <zhutianqi@weizoom.com>
2016-05-06 13:46:30 +0800

    [f_mall_product_search][朱天琦]实现bill查看搜索框bdd

---
+++ features/steps/product_list.step.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from behave import *
+
+@then(u"{user}'{action}'获得商品搜索框")
+def step_impl(context,user,action):
+	response = context.client.get('/mall/products/?category_id=0')
+	data = response.data
+	expected = True if action == u'能' else False
+	actual = data['mall_config']['show_product_search']
+	assert expected == actual

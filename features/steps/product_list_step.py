74f1e3d033896e5ed198da00b8cc753a5a615026
朱天琦 <zhutianqi@weizoom.com>
2016-05-06 15:44:32 +0800

    [f_mall_product_search][朱天琦]update

--- features/steps/product_list_step.py
+++ features/steps/product_list_step.py
@@ -3,7 +3,7 @@
 from behave import *
 
 @then(u"{user}'{action}'获得商品搜索框")
-def step_impl(context,user,action):
+def step_impl(context, user, action):
 	response = context.client.get('/mall/products/?category_id=0')
 	data = response.data
 	expected = True if action == u'能' else False

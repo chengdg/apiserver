7eb7b17975a12eedcd4efce4d2727cdfff290c0a
朱天琦 <zhutianqi@weizoom.com>
2016-07-18 14:02:10 +0800

    [f_mall_10290][朱天琦]update

--- features/h5/webapp/user_product_list.feature
+++ features/h5/webapp/user_product_list.feature
@@ -6,7 +6,7 @@ Feature:用户手机端商品列表分页
 Background:
 	Given 重置'weapp'的bdd环境
 	Given jobs登录系统::weapp
-	When jobs已添加商品分类
+	When jobs已添加商品分类::weapp
 		"""
 		[{
 			"name": "分类1"
@@ -38,13 +38,13 @@ Background:
 		"""
 	When bill关注jobs的公众号
 	Given jobs登录系统::weapp
-	Then jobs能获取商品列表::weapp
-		| name    | created_at       | display_index |
-		| 商品3   | 2015-08-01 08:00 |  0            |
-		| 商品2   | 2015-07-01 08:00 |  0            |
-		| 商品1   | 2015-06-01 08:00 |  0            |
+#	Then jobs能获取商品列表::weapp
+#		| name    | created_at       | display_index |
+#		| 商品3   | 2015-08-01 08:00 |  0            |
+#		| 商品2   | 2015-07-01 08:00 |  0            |
+#		| 商品1   | 2015-06-01 08:00 |  0            |
 
-@app @buy @productList
+@app @buy @productList @ztqb
 Scenario:1 手机端商品列表分页-全部商品
 	When bill访问jobs的webapp
 	And bill浏览jobs的webapp的'全部'商品列表页
@@ -70,7 +70,8 @@ Scenario:1 手机端商品列表分页-全部商品
 		"""
 		{
 			"count_per_page":2,
-			"cur_page":2
+			"cur_page":2,
+			"categories":"全部"
 		}
 		"""
 	Then bill获得webapp商品列表
@@ -83,11 +84,11 @@ Scenario:1 手机端商品列表分页-全部商品
 	#修改商品排序后，查看手机端商品列表
 		Given jobs登录系统::weapp
 		When jobs更新'商品1'商品排序1::weapp
-		Then jobs能获取商品列表::weapp
-			| name    | created_at       | display_index |
-			| 商品1   | 2015-06-01 08:00 |  1            |
-			| 商品3   | 2015-08-01 08:00 |  0            |
-			| 商品2   | 2015-07-01 08:00 |  0            |
+#		Then jobs能获取商品列表::weapp
+#			| name    | created_at       | display_index |
+#			| 商品1   | 2015-06-01 08:00 |  1            |
+#			| 商品3   | 2015-08-01 08:00 |  0            |
+#			| 商品2   | 2015-07-01 08:00 |  0            |
 		When bill访问jobs的webapp
 		And bill浏览jobs的webapp的'全部'商品列表页
 		And bill设置商品列表分页查询参数
--- features/steps/mall_h5_steps.py
+++ features/steps/mall_h5_steps.py
@@ -19,6 +19,25 @@ def step_impl(context, webapp_user_name, webapp_owner_name, category_name):
 	response = context.client.get(bdd_util.nginx(url), follow=True)
 	context.response = response
 
+
+
+
+@when(u"{webapp_user}设置商品列表分页查询参数")
+def step_impl(context, webapp_user):
+	args = json.loads(context.text)
+	context.count_per_page = args['count_per_page']
+	context.cur_page = args['cur_page']
+	category_name = args.get('categories','')
+	if category_name:
+		if category_name == u"全部":
+			context.category_id = 0
+		else:
+			context.category_id = mall_models.ProductCategory.select().dj_where(name=category_name).first().id
+		print('---------p',category_name,context.category_id)
+
+	# context.category_id = args['count_per_page']
+
+
 @then(u"{webapp_user_name}获得webapp商品列表")
 def step_impl(context, webapp_user_name):
 	if context.table:
@@ -28,14 +47,58 @@ def step_impl(context, webapp_user_name):
 			expected.append(promotion)
 	else:
 		expected = json.loads(context.text)
-	if hasattr(context, 'searching_product_name') and context.searching_product_name:
-		searching_product_name = context.searching_product_name
-		url = '/mall/products/?woid=%s&category_id=%s&product_name=%s' % (context.webapp_owner_id, 0,searching_product_name)
-		response = context.client.get(bdd_util.nginx(url), follow=True)
-		actual = response.data['products']
-		context.searching_product_name = None
+
+	if hasattr(context, 'count_per_page'):
+		count_per_page = context.count_per_page
+		del context.count_per_page
+
 	else:
-		actual = context.response.data['products']
+		count_per_page = 100
+
+	if hasattr(context, 'category_id'):
+		category_id = context.category_id
+		print('---------category_id',category_id)
+		del context.category_id
+
+	else:
+		category_id = 100
+
+	if hasattr(context, 'cur_page'):
+		cur_page = context.cur_page
+		del context.cur_page
+	else:
+		cur_page = 1
+
+	if hasattr(context, 'searching_product_name'):
+		searching_product_name = context.searching_product_name
+		del searching_product_name
+	else:
+		searching_product_name = ''
+
+	url = '/mall/products/?woid={}&category_id={}&product_name={}&cur_page={}&count_per_page={}'.format(context.webapp_owner_id, category_id, searching_product_name,cur_page,count_per_page)
+
+	response = context.client.get(bdd_util.nginx(url), follow=True)
+
+
+
+	actual = response.data['products']
+	print('---------------ac',actual)
 	for product in actual:
 		product['price'] = float('%.2f' % float(product['display_price']))
 	bdd_util.assert_list(expected, actual)
+
+
+	# for product in actual:
+	# 	product['price'] = float('%.2f' % float(product['display_price']))
+	#
+	# if hasattr(context, 'searching_product_name') and context.searching_product_name:
+	# 	searching_product_name = context.searching_product_name
+	# 	url = '/mall/products/?woid=%s&category_id=%s&product_name=%s' % (context.webapp_owner_id, 0,searching_product_name)
+	# 	response = context.client.get(bdd_util.nginx(url), follow=True)
+	# 	actual = response.data['products']
+	# 	context.searching_product_name = None
+	# else:
+	# 	actual = context.response.data['products']
+	# for product in actual:
+	# 	product['price'] = float('%.2f' % float(product['display_price']))
+	# bdd_util.assert_list(expected, actual)

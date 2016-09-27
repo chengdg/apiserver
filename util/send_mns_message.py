b6a65a70470b88722dfbb1f6a202fa2c07f94bf1
郭玉成 <guoyucheng@weizoom.com>
2016-09-27 17:27:42 +0800

    delete print

--- util/send_mns_message.py
+++ util/send_mns_message.py
@@ -6,5 +6,4 @@ from bdem import msgutil
 
 
 def send_mns_message(topic_name, message_name, data):
-	print ">>>>>>>>>>>>>.11111,",topic_name,message_name,data
 	msgutil.send_message(topic_name, message_name, data)

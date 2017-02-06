0aab6c9308226db144c3b2bcbbf254accf078473
robert <chenru@weizoom.com>
2017-02-06 10:00:49 +0800

    改进service基础文件

---
+++ commands/inspect_service.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+
+import os
+import json
+
+from util.command import BaseCommand
+
+
+class Command(BaseCommand):
+	help = "inspect service: 1. rebuild db, 2. init db"
+	args = ''
+	
+	def handle(self, **options):
+		if os.environ.get('_IS_SERVICE_IN_CONTAINER', '0') != '1':
+			print '[error]: inspect service must invoked in container'
+			import sys
+			sys.exit(1)
+
+		import servicecli
+		servicecli.inspect_service_in_container()
--- start_service.sh
+++ start_service.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 PORT=${1:-8001}
 
+if [ "${_SERVICE_HOST_PORT}" == "" ]; then
+	__IGNORE=1 #do nothing
+else
+	PORT=${_SERVICE_HOST_PORT}
+fi
+
 # prepare service
 CAN_START_SERVICE=`python <<END
 import servicecli
@@ -11,16 +17,14 @@ if [ "${CAN_START_SERVICE}" == "false" ]; then
 	exit 2
 fi
 
+if [ "$_PROCESS_COUNT" == "" ]; then
+	_PROCESS_COUNT=10
+fi
+echo "process count: ${_PROCESS_COUNT}"
+
 # start service
-if [ "$_WEIZOOM_PRODUCTION" == "1" ]; then
-	uwsgi service.ini
+if [ "$_USE_WSGI_PROTOCAL" == "1" ]; then
+	uwsgi --processes ${_PROCESS_COUNT} --socket 0.0.0.0:${PORT} service.ini
 else
 	python manage.py runserver 0.0.0.0 $PORT
 fi
-
-
-
-#trap cleanup 2
-#while true; do
-# python manage.py runserver 0.0.0.0 8001
-#done

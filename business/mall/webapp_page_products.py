62155e8ccb33c2744ac215b04994de5abc34babd
charles <qiaozhanlei@weizoom.com>
2017-03-30 15:57:19 +0800

    web_app_page_for_doris

--- business/mall/webapp_page_products.py
+++ business/mall/webapp_page_products.py
@@ -35,30 +35,40 @@ class WebAppPageProducts(business_model.Model):
 		self.webapp_user = webapp_user
 		
 	def get_by_product_ids(self, product_ids=None):
-		# CachedSimpleProduct商品规格已经经过改价的处理,business_product.price_info中的就是展示的(包括改价后的平台)
-		# products = [CachedSimpleProduct._get_from_db(product_id, self.webapp_owner) for product_id in product_ids]
-		db_products = mall_models.Product.select().dj_where(id__in=product_ids)
-		products = []
-		for db_product in db_products:
-			product = Product.from_model({"webapp_owner": self.webapp_owner,
-									 	  'model': db_product,
-									 	  "fill_options": {'with_price': True,
-														   "with_product_promotion": True},
-										  })
-			products.append({
-				'id': db_product.id,
-				'name': db_product.name,
-				'is_deleted': db_product.is_deleted,
-				'thumbnails_url': db_product.thumbnails_url,
-				# 商品的规格已经处理过改价
-				'display_price': product.price_info['display_price'],
-				'is_member_product': product.is_member_product,
-				'supplier': product.supplier,
-				'promotion_js': product.promotion.to_dict() if product.promotion else None,
-				'integral_sale': product.integral_sale.to_dict() if product.integral_sale else None,
-			})
-		
-		return products
+		results = []
+		for product_id in product_ids:
+			temp_key = 'product_simple_detail_{pid:%s}' % product_id
+			cache_simple_product = cache_util.get_cache(temp_key)
+			if cache_simple_product:
+				# 如果是缓存中获取的,需要判断一下是否有改价
+				price = cache_util.get_cache('customized_price_{wo:%s}_{pid:%s}'% (self.webapp_user.id, product_id))
+				if price:
+					cache_simple_product['display_price'] = price
+				results.append(cache_simple_product)
+			else:
+				db_product = mall_models.Product.select().dj_where(id=product_id).first()
+				product = Product.from_model({"webapp_owner": self.webapp_owner,
+											  'model': db_product,
+											  "fill_options": {'with_price': True,
+															   "with_product_promotion": True},
+											  })
+				cache_product = {
+					'id': product.id,
+					'name': product.name,
+					'is_deleted': product.is_deleted,
+					'thumbnails_url': product.thumbnails_url,
+					# 商品的规格已经处理过改价
+					'display_price': product.price_info['display_price'],
+					'is_member_product': product.is_member_product,
+					'supplier': product.supplier,
+					'categories': [],
+					'promotion_js': product.promotion.to_dict() if product.promotion else None,
+					'integral_sale': product.integral_sale.to_dict() if product.integral_sale else None,
+				}
+				temp_key = 'product_simple_detail_{pid:%s}' % product_id
+				cache_util.set_cache(temp_key, cache_product)
+				results.append(cache_product)
+		return results
 	
 	def get_by_category(self, category_id=None, count_per_page=None):
 		product_ids = self.get_product_ids_from_category(category_id, count_per_page)
@@ -86,7 +96,11 @@ class WebAppPageProducts(business_model.Model):
 					product_pools = mall_models.ProductPool.select().dj_where(woid=self.webapp_user.id,
 																			  status=mall_models.PP_STATUS_ON)\
 						.order_by(mall_models.ProductPool.display_index, mall_models.ProductPool.sync_at.desc)
-					return [pool.product_id for pool in product_pools][:count_per_page]
+					if product_pools.count() > 0:
+						
+						return [pool.product_id for pool in product_pools][:count_per_page]
+					else:
+						return []
 				else:
 					products = mall_models.Product.select().dj_where(owner_id=self.webapp_user.id,
 																	 shelve_type=mall_models.PRODUCT_SHELVE_TYPE_ON)
